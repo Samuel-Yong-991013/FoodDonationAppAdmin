40ee23aed71128d92e36c82a4fd562557dc4b053
sam <samuelyong91@gmail.com>
2021-11-16 22:37:01 +0800

    made changes to register activity

--- app/src/main/java/com/example/fooddonationappadmin/RegisterActivity.kt
+++ app/src/main/java/com/example/fooddonationappadmin/RegisterActivity.kt
@@ -3,7 +3,9 @@ package com.example.fooddonationappadmin
 import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.widget.Toast
 import com.example.fooddonationappadmin.databinding.ActivityMainBinding
 import com.example.fooddonationappadmin.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -15,20 +17,26 @@ class RegisterActivity : AppCompatActivity() {
     val db = Firebase.firestore
     private lateinit var binding: ActivityRegisterBinding
 
+    private val registerEmail = binding.emailRegister.text.toString()
+    private val registerUsername = binding.usernameRegister.text.toString()
+    private val registerPassword = binding.passwordRegister.text.toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         binding.registerBtn.setOnClickListener{
-            registerUser()
+            if(!TextUtils.isEmpty(registerEmail) && !TextUtils.isEmpty(registerUsername) && !TextUtils.isEmpty(registerPassword)){
+                registerUser()
+            }else{
+                Toast.makeText(this, "Please insert all the fields", Toast.LENGTH_LONG).show()
+            }
+
         }
     }
 
+    //Function for registering user
     fun registerUser(){
-        var registerEmail = binding.emailRegister.text.toString()
-        var registerUsername = binding.usernameRegister.text.toString()
-        var registerPassword = binding.passwordRegister.text.toString()
-
         val registerNewUser = hashMapOf(
             "email" to registerEmail,
             "name" to registerUsername,
