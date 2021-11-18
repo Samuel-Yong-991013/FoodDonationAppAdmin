92234d384c662a8a881fdf3b10312111c888d3b3
sam <samuelyong91@gmail.com>
2021-11-18 23:40:31 +0800

    tried to fix toString() problem

--- app/src/main/java/com/example/fooddonationappadmin/LoginActivity.kt
+++ app/src/main/java/com/example/fooddonationappadmin/LoginActivity.kt
@@ -4,65 +4,80 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Patterns
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.fooddonationappadmin.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth;
-    val db = Firebase.firestore
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-
-        val loginEmail = binding.emailLogin.text.toString()
-        val loginName = binding.usernameLogin.text.toString()
-        val loginPassword = binding.passwordLogin.text.toString()
-
         //Initialise Firebase Auth
         auth = Firebase.auth
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        val loginEmail : String = binding.emailLogin.text.toString()
+        val loginPassword : String = binding.passwordLogin.text.toString()
+
+//        val loginEmail = findViewById<EditText>(R.id.emailLogin).text
+//        val loginPassword = findViewById<EditText>(R.id.passwordLogin).text
 
         //Run this function when loginBtn is clicked
         binding.loginBtn.setOnClickListener{
-//            startActivity(Intent( this@LoginActivity, MainActivity::class.java))
-
             //Check if fields are empty
-            if( !TextUtils.isEmpty(loginEmail)
-                && !TextUtils.isEmpty(loginName)
-                && !TextUtils.isEmpty(loginPassword) ) {
+            if( !TextUtils.isEmpty(binding.emailLogin.text)
+                && !TextUtils.isEmpty(binding.passwordLogin.text) ) {
 
-                //Function to check if the email is valid
-                if(!Patterns.EMAIL_ADDRESS.matcher(loginEmail).matches()) {
-                    loginUser()
-                }
+                Toast.makeText(
+                    this@LoginActivity,
+                    "$loginEmail $loginPassword",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                //If fields are filled in, attempt to log in the user
+                auth.signInWithEmailAndPassword(binding.emailLogin.text.toString(), binding.emailLogin.text.toString())
+                    .addOnCompleteListener { task ->
+                        //Show message that login is successful and move user to MainActivity
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "You are logged in successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            val intent =
+                                Intent(this@LoginActivity, MainActivity::class.java)
+                            intent.flags =
+                                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            intent.putExtra(
+                                "user_id",
+                                FirebaseAuth.getInstance().currentUser!!.uid
+                            )
+                            intent.putExtra("email_id", loginEmail)
+                            startActivity(intent)
+                            finish()
+
+                        }else{
+                            //Show error message if login fails
+                            Toast.makeText(
+                                this@LoginActivity,
+                                task.exception!!.message.toString(),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
             }else{
-                Toast.makeText(this@LoginActivity, "Please fill in all the fields", Toast.LENGTH_LONG).show()
+                //If the fields are empty
+                Toast.makeText(this@LoginActivity, "Please fill in all the fields", Toast.LENGTH_SHORT).show()
             }
         }
-    }
-
-    // FUNCTIONS
-    private fun loginUser(){
-
-        val loginEmail = binding.emailLogin.text.toString()
-        val loginName = binding.usernameLogin.text.toString()
-        val loginPassword = binding.passwordLogin.text.toString()
-
-        val email = loginEmail.trim{ it <= ' ' }
-        val username = loginName.trim{ it <= ' ' }
-        val password = loginPassword.trim{ it <= ' ' }
-
     }
 
 }
